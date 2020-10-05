5a6e5213471bb8f83e245bc68014a8aec279fa76
AimarKarrik <71828910+AimarKarrik@users.noreply.github.com>
2020-10-05 18:29:31 +0300

    Failid
    Lisasin TTT failid githubi

---
+++ TTT.py
@@ -0,0 +1,9 @@
+star = [" ", " ", " ", " ", " "," ", " ", " ", " "]
+
+victory = 0
+
+field = f" {star[0]} │ {star[1]} │ {star[2]} \n───────────\n {star[3]} │ {star[4]} │ {star[5]} \n───────────\n {star[6]} │ {star[7]} │ {star[8]} "
+player = "Mägnija 1"
+player2 = "Mägnija 2"
+
+while victory < 1:
---
+++ test.py
@@ -0,0 +1,4 @@
+lis = ["üks", "kaks", "kolm"]
+print(lis)
+lis[1] = "2"
+print(lis)
