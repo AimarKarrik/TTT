ceb29a6a884dad704145d53565ec54045bf223bd
AimarKarrik <71828910+AimarKarrik@users.noreply.github.com>
2020-10-05 23:17:10 +0300

    Update TTT.py

--- TTT.py
+++ TTT.py
@@ -1,8 +1,9 @@
-star = [" ", " ", " ", " ", " "," ", " ", " ", " "]
+slots = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-victory = 0
-
-field = f" {star[0]} │ {star[1]} │ {star[2]} \n───────────\n {star[3]} │ {star[4]} │ {star[5]} \n───────────\n {star[6]} │ {star[7]} │ {star[8]} "
+victory1 = False
+victory2 = False
+field = f" {slots[0]} │ {slots[1]} │ {slots[2]} \n───────────\n {slots[3]} │ {slots[4]} │ {slots[5]} \n───────────\n {slots[6]} │ {slots[7]} │ {slots[8]} "
 player = "Mägnija 1"
 player2 = "Mägnija 2"
+
 
